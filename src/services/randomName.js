3a5362b70b52ed8262acf9ed8709073d82972a7f
Denis Tišljar <denis.tisljar@gmail.com>
2023-03-07 20:49:21 +0100

    Izbrisani neiskorišteni context i random name funkcija

--- src/services/randomName.js
+++
@@ -1,9 +0,0 @@
-const randomName = () => {
-    const adjectives = ["Autumn", "Hidden", "Bitter", "Misty", "Dark"];
-    const nouns = ["Waterfall", "River", "Breeze", "Moon", "Rain", "Sea", "Morning","Snow"];
-    const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-    const noun = nouns[Math.floor(Math.random() * nouns.length)];
-    return adjective + noun;
-}
-
-export default randomName;
